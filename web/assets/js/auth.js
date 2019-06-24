ab403f509f71845d207e4630a70a06b8ddb4bb28
Dennis Kwabena Bilson Jr <41457025+Quabynah-Codelabs@users.noreply.github.com>
2019-06-24 05:03:09 +0000

    Added key down event listener to admin auth

--- web/assets/js/auth.js
+++ web/assets/js/auth.js
@@ -1,19 +1,69 @@
-$(document).ready(function() {});
+$(document).ready(function() {
+  // Hide loading
+  showLoading(false);
 
-function onSuccess(googleUser) {
-  console.log("Logged in as: " + googleUser.getBasicProfile().getName());
-}
-function onFailure(error) {
-  console.log(error);
-}
-function renderButton() {
-  gapi.signin2.render("my-signin2", {
-    scope: "profile email",
-    width: 240,
-    height: 50,
-    longtitle: true,
-    theme: "dark",
-    onsuccess: onSuccess,
-    onfailure: onFailure
+  // Listen for key down events
+  document.addEventListener("keydown", function(event) {
+    if (event.keyCode == 13) {
+      login();
+    }
   });
-}
+});
+
+const showLoading = state => {
+  if (state) {
+    $("#loading").show();
+  } else {
+    $("#loading").hide();
+  }
+};
+
+// Email and password
+const login = () => {
+  // Get the fields
+  var email = $("#auth_email");
+  var password = $("#auth_password");
+
+  if (validator.isEmail(email.val()) && !validator.isEmpty(password.val())) {
+    showLoading(true);
+    email.prop("disabled", true);
+    password.prop("disabled", true);
+    $("#login_button").attr("disabled", true);
+
+    // Do some login
+  } else {
+    alert("Please enter your password");
+  }
+};
+
+// Google Auth
+const googleOAuth = () => {
+  // Get provider
+  var provider = new firebase.auth.GoogleAuthProvider();
+
+  // Add scope
+  provider.addScope("https://www.googleapis.com/auth/contacts.readonly");
+
+  //   Authenticate user
+  firebase
+    .auth()
+    .signInWithPopup(provider)
+    .then(function(result) {
+      // This gives you a Google Access Token. You can use it to access the Google API.
+      var token = result.credential.accessToken;
+      // The signed-in user info.
+      var user = result.user;
+      window.localStorage.setItem("admin-key", user.uid);
+      window.location.href = "./dashboard.html";
+    })
+    .catch(function(error) {
+      // Handle Errors here.
+      var errorCode = error.code;
+      var errorMessage = error.message;
+      // The email of the user's account used.
+      var email = error.email;
+      // The firebase.auth.AuthCredential type that was used.
+      var credential = error.credential;
+      alert(`Auth credentials: ${credential}`);
+    });
+};
