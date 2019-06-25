cfc6d891a6af54313de365dccf08446c8ef78fb4
Dennis Kwabena Bilson Jr <41457025+Quabynah-Codelabs@users.noreply.github.com>
2019-06-25 01:39:18 +0000

    Added info tab

---
+++ web/resume/assets/js/info.js
@@ -0,0 +1,54 @@
+var user = [
+  {
+    name: "Quabynah Bilson",
+    position: "CEO &amp; Lead Developer Advocate",
+    email: "codelbas.quabynah@gmail.com",
+    phone: "+233554022344",
+    desc:
+      "I am experienced in leveraging agile frameworks to provide a robust synopsis for high level overviews. Iterative approaches to corporate strategy foster collaborative thinking to further the overall value proposition.",
+    experience: [
+      {
+        title: "Mobile Apps developer",
+        company: "Zeniteck IT Solutions",
+        duration: "2018-present"
+      },
+      {
+        title: "Mobile Applications tutor",
+        company: "Department of Computer Science, UG, Legon",
+        duration: "2018-present"
+      },
+      {
+        title: "Founder &amp; Lead developer advocate",
+        company: "Quabynah Codelabs",
+        duration: "2019-present"
+      },
+      {
+        title: "Mobile Banker",
+        company: "Waxson Financial Services",
+        duration: "2013-2014"
+      }
+    ],
+    education: [
+      {
+        institution: "University of Ghana",
+        course: "Computer Science",
+        type: "Bachelor of Arts",
+        duration: "2014-2018",
+        gpa: "2.89"
+      },
+      {
+        institution: "Odorgonno Senior High School",
+        course: "Business",
+        type: "WASSCE",
+        duration: "2009-2013",
+        gpa: "8.0"
+      }
+    ],
+    skills: [],
+    interests:
+      "Apart from being a web developer, I enjoy most of my time being outdoors. In the winter, I am an avid skier and novice ice climber. During the warmer months here in Colorado, I enjoy mountain biking, free climbing, and kayaking. When forced indoors, I follow a number of sci-fi and fantasy genre movies and television shows, I am an aspiring chef, and I spend a large amount of my free time exploring the latest technology advancements in the front-end web development world.",
+    awards: []
+  }
+];
+
+$(document).ready(function() {});
