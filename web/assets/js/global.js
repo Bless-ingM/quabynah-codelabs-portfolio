3a59e624aec593c8d1d66ab1893e286c51f5c3bd
Dennis Kwabena Bilson Jr <41457025+Quabynah-Codelabs@users.noreply.github.com>
2019-06-29 07:04:49 +0000

    Updated stuff

--- web/assets/js/global.js
+++ web/assets/js/global.js
@@ -12,6 +12,11 @@ var firebaseConfig = {
 // Initialize Firebase
 firebase.initializeApp(firebaseConfig);
 
+// Initialize braintree
+// braintree.client.create({
+// 
+// });
+
 // Load page
 $(document).ready(function() {
   var scroll = new SmoothScroll('a[href*="#"]', {
@@ -20,4 +25,21 @@ $(document).ready(function() {
   });
 
   console.log(firebase.auth().currentUser);
+  window.onscroll = function() {
+    scrollFunction();
+  };
 });
+
+function scrollFunction() {
+  if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+    document.getElementById("scroll_button").style.display = "block";
+  } else {
+    document.getElementById("scroll_button").style.display = "none";
+  }
+}
+
+// When the user clicks on the button, scroll to the top of the document
+function topFunction() {
+  document.body.scrollTop = 0; // For Safari
+  document.documentElement.scrollTop = 0; // For Chrome, Firefox, IE and Opera
+}
